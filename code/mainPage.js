20b9084c4ba9ce68681d02697bda4df4d21e8d71
keltomerka <jana.blinska@gmail.com>
2023-11-02 11:41:23 +0100

    v2

---
+++ code/mainPage.js
@@ -0,0 +1,6 @@
+import{userName, email, tel, passwrd} from "./welcomePage.js";
+const userInfo = document.getElementById("iconhead")
+console.log(userName, email, tel, passwrd);
+userInfo.addEventListener("click", () =>{
+    
+})
--- code/welcomePage.js
+++ code/welcomePage.js
@@ -1,4 +1,6 @@
+//вітальня сторінка, реєстрація
 const btnSing = document.getElementById("btn-sing")
+const inputs = document.querySelectorAll(".welInp")
 btnSing.addEventListener("click", () => {
     const modal = document.getElementById("modal")
     modal.classList.add("displayFlex")
@@ -10,5 +12,12 @@ btnClose.addEventListener("click", () => {
 const btnCreate = document.getElementById("btnCreate")
 btnCreate.addEventListener("click", () => {
     window.open("mainPage.html")
-    window.close("welcomePage.html")
+    // window.close("welcomePage.html")
+    const userName = inputs[0].value;
+    const email = inputs[1].value;
+    const tel = inputs[2].value;
+    const passwrd = inputs[3].value;
+    console.log(userName, email, tel, passwrd);
 })
+
+ 
